197875e32d216d37e8a5a150c9dc0bce187c295e
NivethaRS <nivethasivakumar1998@gmail.com>
2024-07-02 17:12:24 -0400

    JULY2

---
+++ src/test/java/July2/Assertions.java
@@ -0,0 +1,29 @@
+package July2;
+import  org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+
+
+
+public class Assertions {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://spicejet.com"); //URL in the browser
+
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+//Assert.assertFalse(true);System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+
+    }
+}
---
+++ src/test/java/July2/AutosuggestiveDropdown.java
@@ -0,0 +1,28 @@
+package July2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class AutosuggestiveDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        /* driver.manage().window().fullscreen(); */
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+     Thread.sleep(2000);
+        List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item']a"));
+        for(WebElement option: options)
+        {
+            if(option.getText().equalsIgnoreCase("India"))
+            {
+            option.click();
+            break;
+            }
+        }
+        }
+}
